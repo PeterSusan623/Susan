a35397f6c75d4e5189f04a15a9d88ef68f5c7732
PeterSusan623 <107458427+PeterSusan623@users.noreply.github.com>
2022-09-02 22:41:36 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -23,22 +23,28 @@ TOUSER = ['o-kLm6OySXaJHztFPHmzMb9uobE4',     #peter
           'o-kLm6Kbbb1694nlt4nyrzRbOO9I',     #小不点   5
           'o-kLm6JMczPEg622U2NAdXHMX5D8',     #佟颖     6
           'o-kLm6JGIh7b6mrDij1rhtg-7CRw',     #老张
+          'o-kLm6Da1B1XcbrufuUWmaL4Zi84',     #香香
+          'o-kLm6G4MaMuW7kVYgXGKrPMJhf8',     #绿道项目负责人
           'o-kLm6OCrDQjWaWcDwf32-JXDal0']     #测试
+
 
 a = 0
 
 class SendMessage(object):
     # print("a=",a)
     # 消息接收者
-    TOUSER = ['o-kLm6OySXaJHztFPHmzMb9uobE4',     #peter
-              'o-kLm6KvU4BSuxiKz3tEwszYrSzo',     #潮弟
-              'o-kLm6AW-Q9V9wHFiZjBmH2E0koM',     #安儿子
-              'o-kLm6NW13Og_V3fxfCqFWlEQDOo',     #锦城小黄  3
-              'o-kLm6CNSyEvuWkXIhpBvOEJZM7g',     #男神     4
-              'o-kLm6Kbbb1694nlt4nyrzRbOO9I',     #小不点   5
-              'o-kLm6JMczPEg622U2NAdXHMX5D8',     #佟颖     6
-              'o-kLm6JGIh7b6mrDij1rhtg-7CRw',     #老张
-              'o-kLm6OCrDQjWaWcDwf32-JXDal0']     #测试
+TOUSER = ['o-kLm6OySXaJHztFPHmzMb9uobE4',     #peter
+          'o-kLm6KvU4BSuxiKz3tEwszYrSzo',     #潮弟
+          'o-kLm6AW-Q9V9wHFiZjBmH2E0koM',     #安儿子
+          'o-kLm6NW13Og_V3fxfCqFWlEQDOo',     #锦城小黄  3
+          'o-kLm6CNSyEvuWkXIhpBvOEJZM7g',     #男神     4
+          'o-kLm6Kbbb1694nlt4nyrzRbOO9I',     #小不点   5
+          'o-kLm6JMczPEg622U2NAdXHMX5D8',     #佟颖     6
+          'o-kLm6JGIh7b6mrDij1rhtg-7CRw',     #老张
+          'o-kLm6Da1B1XcbrufuUWmaL4Zi84',     #香香
+          'o-kLm6G4MaMuW7kVYgXGKrPMJhf8',     #绿道项目负责人
+          'o-kLm6OCrDQjWaWcDwf32-JXDal0']     #测试
+
     # 消息模板id
     TEMPLATE_ID = 'i--KZsfRnhuLqKclGnAJoSyfrFlPwxAa1wyksPBAwF0'
     # 点击跳转链接（可无）
