a009b8578d1d6a3492d89cd2ea9ceb43edabb7ae
PeterSusan623 <107458427+PeterSusan623@users.noreply.github.com>
2022-10-02 23:34:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ TOUSER = ['o-kLm6OySXaJHztFPHmzMb9uobE4',     #peter
           'o-kLm6JA7oFY4vKqlPyFZZ8rnHuc',     #孙莉     8
           'o-kLm6DiU7LLDa53mnWzD6WaOVVk',     #蒋老板   9
           'o-kLm6IyW1gKeBHFECLyCVr3K6j4',     #抖音小董 10
+          'o-kLm6AJo0OS9HaJwATA3qL6xFwU',     #python   11    河南信阳 30 1
           'o-kLm6JGIh7b6mrDij1rhtg-7CRw',     #老张
           'o-kLm6Da1B1XcbrufuUWmaL4Zi84',     #香香
           'o-kLm6G4MaMuW7kVYgXGKrPMJhf8',     #绿道项目负责人
@@ -207,6 +208,8 @@ class Main(object):
             city_code = '101280101'   #广州
         if a == 10:   #抖音小董所在的城市
             city_code = '101090101'   #石家庄
+        if a == 11:   #python所在的城市 
+            city_code = '101180601'   #信阳
         tqurl = api + city_code
         response = requests.get(tqurl)
         d = response.json()  # 将数据以json形式返回，这个d就是返回的json数据
